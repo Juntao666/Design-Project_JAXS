a1ea0cc4698e5145e9649c995c94a756659390f0
Juntao666 <jl13036@nyu.edu>
2025-02-25 16:20:10 -0500

    modified scripts to set environment variables

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Run your React frontend against a local API server.
-export REACT_APP_URL_PRE="https://<YOUR_SERVERS_CLOUD_ADDRESS_HERE>/"
+# Run your React frontend against a cloud API server.
+export REACT_APP_URL_PRE="http://127.0.0.1:8000"
+echo "Starting React app with CLOUD backend at $REACT_APP_BACKEND_URL..."
 npm start
