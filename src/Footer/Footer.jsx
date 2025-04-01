bd87a0caaa092465f2ec3cd0c2c1985083fbfdb1
Juntao666 <jl13036@nyu.edu>
2025-04-01 19:43:26 -0400

    created a banner

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+      <div className="footer-banner">
+        <div className="footer-text">
+          <h2>Crafted with ❤️ by Our Team</h2>
+          <p>© 2025 All rights reserved.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
